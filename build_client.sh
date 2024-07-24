b8fac5e62573d34d10f4101fc3146468e43c084d
mmm1245 <m.morhac123@gmail.com>
2024-07-24 16:43:10 +0200

    fix

--- build_client.sh
+++ build_client.sh
@@ -1,2 +1,2 @@
-cargo build --target wasm32-unknown-unknowncargo build --release --package hydro_client --target wasm32-unknown-unknown
+cargo build --release --package hydro_client --target wasm32-unknown-unknown
 cp target/wasm32-unknown-unknown/release/hydro_client.wasm hydro_server/host
